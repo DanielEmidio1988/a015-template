bd7aa3cddb3b1e464b98c6bde213c1935e7cc74f
DanielEmidio1988 <daniel.emidio.silva@gmail.com>
2022-09-12 11:27:54 -0300

    Exercicio 12/09

---
+++ script.js
@@ -0,0 +1,65 @@
+// let numeroUsuario
+// let soma = 0
+// while(numeroUsuario !== 0){ 
+//     numeroUsuario = Number(prompt("Insira um numero"))
+//     console.log(numeroUsuario)
+//     soma += numeroUsuario
+//     console.log("Soma: ", soma)
+// }
+// console.log("Soma de todos os numeros: ", soma)
+
+// let numUsuario = 0
+// let soma = 0
+// for (let x = 0; x !== 5; x++){
+//     numUsuario = Number(prompt("Digite um numero"))
+//     soma += numUsuario
+//     console.log("A soma do ",x,"º numero é: ", soma)
+// } 
+
+// console.log("A soma de todos os numeros é: ", soma)
+
+// let numeroFor = Number(prompt("Digite o numero de vezes"))
+
+// for(let i = 0; i < numeroFor+1; i++){
+//     console.log(i)
+// }
+
+// let numArray = [1,3,5,2,4,6,9]
+
+// for(let i = 0; i <= numArray.length; i++){
+//     console.log(`O numero do indice ${i} é ${numArray[i]}`)
+// }
+
+// let codUsuario = prompt("Digite seu usuário")
+// while(codUsuario !=="A"){
+//     if(codUsuario === "B"){
+//         console.log("B: Usuario comum!")
+//         console.log("Acesso negado")
+//     }else if(codUsuario === "C"){
+//         console.log("C: Usuario assinante!")
+//         console.log("Acesso negado")
+//     }else{
+//         console.log("Usuário não identificado")
+//         console.log("Acesso negado!")}
+//     codUsuario = prompt("Digite seu usuário")
+// }
+// console.log("Acesso Permitido!")
+
+//Exercicio 1
+// let codUsuario = prompt("Digite seu usuário").toUpperCase()
+// while(codUsuario !=="A"){
+//     console.log("Acesso negado!")
+//     codUsuario = prompt("Digite seu usuário").toUpperCase()
+// }
+// console.log("Boas vindas, admin!")
+
+//Exercicio 2, realizar a Tabuada do 2
+// for(let i = 1; i<=10;i++){
+//     console.log("2 * ", i," = ", 2 * i)
+// }
+
+//Exercicio 3. Criar um laço for que recebe um Array de Strings
+// let arrayString = ["Daniel","AmmalB","Pizza","labenu"]
+// for(let i=0;i<arrayString.length;i++){
+//     console.log(arrayString[i].toUpperCase())
+// }
